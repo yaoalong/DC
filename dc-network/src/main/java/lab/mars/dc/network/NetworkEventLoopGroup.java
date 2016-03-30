9de27c5cfead506ac5916967a5c3b02e927dfd14
yaoalong <sishimei@163.com>
2016-03-30 20:53:07 +0800

    完成了网络模块

---
+++ dc-network/src/main/java/lab/mars/dc/network/NetworkEventLoopGroup.java
@@ -0,0 +1,28 @@
+package lab.mars.dc.network;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+/**
+ * Author:yaoalong.
+ * Date:2016/3/3.
+ * Email:yaoalong@foxmail.com
+ */
+public class NetworkEventLoopGroup {
+
+    public static final int NCPU=Runtime.getRuntime().availableProcessors();
+
+    public static final EventLoopGroup bossGroup;
+
+    public static final EventLoopGroup workerGroup;
+
+
+    static{
+        bossGroup=new NioEventLoopGroup(NCPU);
+        workerGroup=new NioEventLoopGroup(NCPU);
+    }
+    public static void shutdown(){
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+}
---
+++ dc-network/src/main/java/lab/mars/dc/network/initializer/TcpChannelInitializer.java
@@ -0,0 +1,24 @@
+package lab.mars.dc.network.initializer;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+public abstract class TcpChannelInitializer extends
+        ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline channelPipeline = ch.pipeline();
+        channelPipeline.addLast(new ObjectEncoder());
+        channelPipeline.addLast(new ObjectDecoder(ClassResolvers
+                .cacheDisabled(null)));
+        init(channelPipeline);
+
+    }
+
+    public abstract void init(ChannelPipeline ch);
+}
